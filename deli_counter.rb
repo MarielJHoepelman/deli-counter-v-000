da81679f4666be34adfb95ed7befe22385467f60
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-15 01:17:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    message = "The line is currently empty."
+  else
+   message = "The line is currently:"
+   katz_deli.each_with_index  do | name, i |
+      message << " #{i + 1}. #{name}"
+    end
+  end
+  puts message
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{array.count} in line."
+end
